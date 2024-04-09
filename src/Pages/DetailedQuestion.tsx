e888a23917819addc4d5adfcc9106d7952d9e46c
BMKellner <benjaminmkellner@gmail.com>
2024-04-09 14:51:41 -0400

    asdasd

--- src/Pages/DetailedQuestion.tsx
+++ src/Pages/DetailedQuestion.tsx
@@ -4,7 +4,7 @@ const DetailedQuestion = () => {
   return (
     <div style={{ backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '10px' }}>
       <h1>Detailed Questions</h1>
-      <p>This is the Detailed Questions page.</p>
+      <p>A detailed career assessment is a thorough evaluation that identifies an individual's strengths, interests, and values related to careers, using tests and questionnaires to guide career planning and decision-making.</p>
     </div>
   );
 };
