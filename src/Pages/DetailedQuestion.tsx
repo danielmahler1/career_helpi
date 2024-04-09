d8002974cf49956625c31bf3499b5214ca2d1c63
BMKellner <benjaminmkellner@gmail.com>
2024-04-09 15:03:46 -0400

    Updated Detailed
    Description complete

--- src/Pages/DetailedQuestion.tsx
+++ src/Pages/DetailedQuestion.tsx
@@ -4,7 +4,11 @@ const DetailedQuestion = () => {
   return (
     <div style={{ backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '10px' }}>
       <h1>Detailed Questions</h1>
-      <p>This is the Detailed Questions page.</p>
+      <p>
+The detailed career assessment is a comprehensive evaluation process designed to help individuals understand their 
+strengths, weaknesses, interests, and values as they relate to the workplace. 
+This assessment typically involves a series of tests and questionnaires that cover a wide range of areas, including
+ personality traits, cognitive abilities, work preferences, and motivational factors.</p>
     </div>
   );
 };
