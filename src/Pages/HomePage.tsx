7265d44364a54ab73ed760ddb1b6165467c9a6ba
danielmahler1 <danielmahler34@gmail.com>
2024-04-09 15:19:52 -0400

    update footer, remove default background

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -9,28 +9,27 @@ interface HomePageProps {
   changeKey: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const HomePage = ({ keyData, setKey, handleSubmit, changeKey }: HomePageProps) => {
+const HomePage = ({ keyData, handleSubmit, changeKey }: HomePageProps) => {
   return (
     <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <p className="name-tag">Nathan Wolf</p>
-        <p className="name-tag">Daniel Mahler</p>
-        <p className="name-tag">Benjamin Kellner</p>
-      </header>
-      <Form onSubmit={handleSubmit}>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" value={keyData} onChange={changeKey}></Form.Control>
-        <br />
-        <Button className="Submit-Button" type="submit">Submit</Button>
-      </Form>
+      <footer className="footer">
+        <Form onSubmit={handleSubmit} className="api-form">
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control type="password" placeholder="Insert API Key Here" value={keyData} onChange={changeKey}></Form.Control>
+          <Button className="submit-button" type="submit">Submit</Button>
+        </Form>
+        <div className="names-container">
+          <p className="name-tag">Nathan Wolf</p>
+          <p className="name-tag">Daniel Mahler</p>
+          <p className="name-tag">Benjamin Kellner</p>
+        </div>
+      </footer>
     </>
   );
 };
+
+
+
 
 export default HomePage;
 export {};
