b081d737bc008d5fbe981a628184d07578b23b78
natew100 <nathanwolf100@gmail.com>
2024-05-01 01:47:31 -0400

    style updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useRef, useState } from "react";
-import { Button, Form, Navbar, Nav, Container } from "react-bootstrap";
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import "./App.css";
 import BasicQuestion from "./Pages/BasicQuestion";
 import DetailedQuestion from "./Pages/DetailedQuestion";
+import About from "./Pages/About";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -72,22 +75,53 @@ const HomePage = ({ keyData, handleSubmit, changeKey }: HomePageProps) => {
   );
 };
 
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/starter_helpi/" style={{ marginLeft: 10 }}>
+        CareerFinder
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ marginRight: 10 }} />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/basic-questions">
+            Basic Quiz
+          </Nav.Link>
+          <Nav.Link as={Link} to="/detailed-questions">
+            Detailed Quiz
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about-us">
+            About
+          </Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link as={Link} to="/results">
+            Results Overview
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
 const Footer = ({ keyData, handleSubmit, changeKey }: FooterProps) => {
   return (
-    <footer className="footer">
-      <Form onSubmit={handleSubmit} className="api-form">
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" value={keyData} onChange={changeKey}></Form.Control>
-        <Button className="submit-button" type="submit">
-          Submit
-        </Button>
+    <Navbar bg="dark" variant="dark" className="justify-content-between text-white footer">
+      <Form onSubmit={handleSubmit} className="footer-form">
+        <InputGroup className="input-group-custom">
+          <InputGroup.Text id="basic-addon1">API Key</InputGroup.Text>
+          <FormControl type="password" placeholder="Insert API Key Here" aria-label="API Key" aria-describedby="basic-addon1" value={keyData} onChange={changeKey} />
+          <Button type="submit" variant="outline-light">
+            Submit
+          </Button>
+        </InputGroup>
       </Form>
-      <div className="names-container">
-        <p className="name-tag">Nathan Wolf</p>
-        <p className="name-tag">Daniel Mahler</p>
-        <p className="name-tag">Benjamin Kellner</p>
-      </div>
-    </footer>
+      <Nav>
+        <Nav.Item className="mx-3">Nathan Wolf</Nav.Item>
+        <Nav.Item className="mx-3">Daniel Mahler</Nav.Item>
+        <Nav.Item className="mx-3">Benjamin Kellner</Nav.Item>
+      </Nav>
+    </Navbar>
   );
 };
 
@@ -105,38 +139,14 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-          <Container>
-            <Navbar.Brand as={Link} to="/starter_helpi/">
-              CareerPath
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/basic-questions">
-                  Basic Quiz
-                </Nav.Link>
-                <Nav.Link as={Link} to="/detailed-questions">
-                  Detailed Quiz
-                </Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Sign Up
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Header />
         <ToastContainer />
         <Routes>
           <Route path="/" element={<Navigate to="/starter_helpi/" replace />} />
           <Route path="/starter_helpi/" element={<HomePage keyData={key} setKey={setKey} handleSubmit={handleSubmit} changeKey={changeKey} />} />
           <Route path="/basic-questions" element={<BasicQuestion />} />
           <Route path="/detailed-questions" element={<DetailedQuestion />} />
+          <Route path="/about-us" element={<About />} />
         </Routes>
         <Footer keyData={key} handleSubmit={handleSubmit} changeKey={changeKey} />
       </div>
