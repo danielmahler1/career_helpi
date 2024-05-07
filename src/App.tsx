32d1b7e52a45121c66ef63d2fba6b04572075306
natew100 <nathanwolf100@gmail.com>
2024-05-07 00:56:27 -0400

    test again

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./Components/Header";
 import HomePage from "./Components/HomePage";
 import BasicQuestion from "./Pages/BasicQuestion";
